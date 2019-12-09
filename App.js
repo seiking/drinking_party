fbf4ace80d3e0fc9876169ebcd30915739bc9472
naouu <naohiro74_ipad@icloud.com>
2019-12-10 04:27:23 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>こんにちは</Text>
+      <Text>こんにちは!</Text>
     </View>
   );
 }
