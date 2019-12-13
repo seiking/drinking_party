af38e84d6f94078c67643f75c159f84d7f8d393e
siking <sei.wakami1924@icloud.com>
2019-12-14 03:56:10 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>こんにちは</Text>
-    </View>
-  );
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>飲み会</Text>
+//       <Text>ゲーム</Text>
+//       <Bbb />
+//     </View>
+//   );
+// }
+
+// const Bbb = () => {
+//   return (
+//     <View>
+//       <Text>ver 1.00</Text>
+//       <Text>ルーレット</Text>
+//     </View>
+//   );
+// };
+
+export default class App extends React.Component {
+  // //コンストラクタ
+  // constructor(props){
+  //   super(props); //必ず呼ぶ
+  // }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title_big}>飲み会</Text>
+        <Text style={styles.title_medium}>ゲーム</Text>
+        {/* <Image
+          style={{ width: 50, height: 50 }}
+          source={require("@expo/snack-static/react-native-logo.png")}
+        /> */}
+        <Text style={styles.title_small}>ver 1.00</Text>
+        <Text>ルーレットaa</Text>
+        <Text>〇〇〇〇</Text>
+        <Text>ルーレットbb</Text>
+        <Text>おまかせ</Text>
+        <Text>古今東西ゲーム</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFECE0",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  title_big: {
+    fontSize: 75,
+    color: "#DB9B88"
+  },
+  title_medium: {
+    fontSize: 56,
+    color: "#DB9B88"
+  },
+  title_small: {
+    fontSize: 19,
+    color: "#DB9B88"
+  }
 });
