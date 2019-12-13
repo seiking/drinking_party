ff38cd05ae438a481d9f50ea4b18b49de650f0ed
naouu <naohiro74_ipad@icloud.com>
2019-12-14 06:11:41 +0900

    add navigation page

--- App.js
+++ App.js
@@ -1,50 +1,18 @@
-// import React from 'react';
-import React, { Component } from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default class App extends React.Component {
-  // //コンストラクタ
-  // constructor(props){
-  //   super(props); //必ず呼ぶ
-  // }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title_big}>飲み会</Text>
-        <Text style={styles.title_medium}>ゲーム</Text>
-        {/* <Image
-          style={{ width: 50, height: 50 }}
-          source={require("@expo/snack-static/react-native-logo.png")}
-        /> */}
-        <Text style={styles.title_small}>ver 1.00</Text>
-        <Text>ルーレットaa</Text>
-        <Text>〇〇〇〇</Text>
-        <Text>ルーレットbb</Text>
-        <Text>おまかせ</Text>
-        <Text>古今東西ゲーム</Text>
-      </View>
-    );
+import HomeScreen from './src/screens/HomeScreen'
+import BombScreen from './src/screens/BombScreen'
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Bomb: BombScreen,
+  },
+  {
+    initialRouteName: 'Home',
   }
-}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFECE0",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  title_big: {
-    fontSize: 75,
-    color: "#DB9B88"
-  },
-  title_medium: {
-    fontSize: 56,
-    color: "#DB9B88"
-  },
-  title_small: {
-    fontSize: 19,
-    color: "#DB9B88"
-  }
-});
+export default createAppContainer(AppNavigator);
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class HomeScreen extends React.Component {
+  // //コンストラクタ
+  // constructor(props){
+  //   super(props); //必ず呼ぶ
+  // }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title_big}>飲み会</Text>
+        <Text style={styles.title_medium}>ゲーム</Text>
+        {/* <Image
+          style={{ width: 50, height: 50 }}
+          source={require("@expo/snack-static/react-native-logo.png")}
+        /> */}
+        <Text style={styles.title_small}>ver 1.00</Text>
+        <Text>ルーレットaa</Text>
+        <Text>〇〇〇〇</Text>
+        <Text>ルーレットbb</Text>
+        <Text>おまかせ</Text>
+        <Text>古今東西ゲーム</Text>
+        <Button
+          title="Go to Test"
+          onPress={() => this.props.navigation.navigate('Bomb')}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFECE0",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title_big: {
+    fontSize: 75,
+    color: "#DB9B88"
+  },
+  title_medium: {
+    fontSize: 56,
+    color: "#DB9B88"
+  },
+  title_small: {
+    fontSize: 19,
+    color: "#DB9B88"
+  }
+});
