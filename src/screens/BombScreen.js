2141a3c2c5a4062c543b327f9f7a68643180957a
naouu <naohiro74_ipad@icloud.com>
2019-12-16 15:54:34 +0900

    Update BombScreen.js

--- src/screens/BombScreen.js
+++ src/screens/BombScreen.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, Button } from 'react-native';
 
 export default class BombScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>爆弾ページ</Text>
+        <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'https://stockmaterial.net/wp/wp-content/uploads/img/other_bomb01_01.png'}}
+        />
+        <Text>15分の3</Text>
+        <Button
+          title="start"
+          onPress={() => console.log('testだよー')}
+        />
+        <Button
+          title="おまかせ"
+          onPress={() => console.log('おまかせ')}
+        />
       </View>
     );
   }
