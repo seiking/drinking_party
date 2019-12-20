3591efd3e4a1529618dc722256fae4349e306c32
naouu <naohiro74_ipad@icloud.com>
2019-12-21 05:32:55 +0900

    add admob

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TouchableHighlight } from "react-native";
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded
+} from 'expo-ads-admob';
 
 export default class HomeScreen extends React.Component {
   // //コンストラクタ
   // constructor(props){
   //   super(props); //必ず呼ぶ
   // }
+  async Interstitial () {
+    if(__DEV__){
+      AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712'); // テスト広告
+    }else{
+      if(Platform.OS === 'ios'){
+        AdMobInterstitial.setAdUnitID('広告ユニットID'); //iOS
+      }else{
+        AdMobInterstitial.setAdUnitID('広告ユニットID'); //android
+      }
+    }
+    await AdMobInterstitial.requestAdAsync();
+    await AdMobInterstitial.showAdAsync();
+  }
+  
   aaa = () => {
     console.log('aaa')
+  }
+  bbb = () => {
+    console.log('bbb')
   }
   render() {
     return (
@@ -18,16 +41,16 @@ export default class HomeScreen extends React.Component {
           <Text style={styles.title_small}>ver 1.00</Text>
         </View>
         <View style={styles.bottom_button}>
-        <TouchableHighlight style={styles.button} onPress={this.aaa} >
+        <TouchableHighlight style={styles.button} onPress={this.aaa} underlayColor={'#FFECE0'} activeOpacity={0.7} underlayColor={'#FDF9F7'}>
           <Text style={styles.bottom_button_text}>ルーレット</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={() => this.props.navigation.navigate('Bomb')} >
+        <TouchableHighlight style={styles.button} onPress={() => this.props.navigation.navigate('Bomb')} underlayColor={'#FDF9F7'}>
           <Text style={styles.bottom_button_text}>爆弾ゲーム</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={this.aaa} >
+        <TouchableHighlight style={styles.button} onPress={this.aaa} underlayColor={'#FDF9F7'}>
           <Text style={styles.bottom_button_text}>王様ゲーム</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={this.aaa} >
+        <TouchableHighlight style={styles.button} onPress={this.Interstitial} underlayColor={'#FDF9F7'}>
           <Text style={styles.bottom_button_text}>古今東西ゲーム</Text>
         </TouchableHighlight>
         </View>
