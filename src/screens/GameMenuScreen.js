d0c9ed449acd6d5df231a12b53c1ce966267354b
naouu <naohiro74_ipad@icloud.com>
2019-12-22 09:03:07 +0900

    Update GameMenuScreen.js

--- src/screens/GameMenuScreen.js
+++ src/screens/GameMenuScreen.js
@@ -22,36 +22,59 @@ export default class GameMenuScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={this.aaa}
-          underlayColor={'#FFECE0'}
-          activeOpacity={0.7}
-          underlayColor={'#FDF9F7'}
-        >
-          <Text style={styles.bottom_button_text}>ルーレット</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={() => this.props.navigation.navigate('Bomb')}
-          underlayColor={'#FDF9F7'}
-        >
-          <Text style={styles.bottom_button_text}>爆弾ゲーム</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={this.aaa}
-          underlayColor={'#FDF9F7'}
-        >
-          <Text style={styles.bottom_button_text}>王様ゲーム</Text>
-        </TouchableHighlight>
-        <TouchableHighlight
-          style={styles.button}
-          onPress={this.Interstitial}
-          underlayColor={'#FDF9F7'}
-        >
-          <Text style={styles.bottom_button_text}>古今東西ゲーム</Text>
-        </TouchableHighlight>
+      <View>
+        <View style={styles.game_lists}>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.aaa}
+            underlayColor={'#FFECE0'}
+            activeOpacity={0.7}
+            underlayColor={'#FDF9F7'}
+          >
+            <Text style={styles.bottom_button_text}>ルーレット</Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Bomb')}
+            underlayColor={'#FDF9F7'}
+          >
+          <View style= {{alignItems: 'center'}}>
+            <Text style={styles.bottom_button_text}>爆弾</Text>
+            <Text style={styles.bottom_button_text}>ゲーム</Text>
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.aaa}
+            underlayColor={'#FDF9F7'}
+          >
+          <View style= {{alignItems: 'center'}}>
+            <Text style={styles.bottom_button_text}>王様</Text>
+            <Text style={styles.bottom_button_text}>ゲーム</Text>
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.Interstitial}
+            underlayColor={'#FDF9F7'}
+          >
+          <View style= {{alignItems: 'center'}}>
+            <Text style={styles.bottom_button_text}>古今東西</Text>
+            <Text style={styles.bottom_button_text}>ゲーム</Text>
+          </View>
+          </TouchableHighlight>
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.Interstitial}
+            underlayColor={'#FDF9F7'}
+          >
+          <View style= {{alignItems: 'center'}}>
+            <Text style={styles.bottom_button_text}>不平等</Text>
+            <Text style={styles.bottom_button_text}>割り勘</Text>
+          </View>
+          </TouchableHighlight>
+        </View>
+        </View>
       </View>
     )
   }
@@ -63,23 +86,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   },
-  body: {
-    flex: 3,
-    backgroundColor: '#FFECE0',
-    width: '100%'
-  },
-  bottom_button: {
-    flex: 4,
-    // backgroundColor: 'red',
-    width: '100%'
+  game_lists: {
+    flex:1, flexDirection:'row', flexWrap: 'wrap',
+    // backgroundColor: 'blue',
+    marginTop: 30,
+    justifyContent: 'space-evenly',
   },
   bottom_button_text: {
     color: '#DB9B88'
   },
   button: {
-    flex: 1,
-    backgroundColor: 'rgba(243,223,223,0.38)',
-    width: '100%',
-    height: '10%'
-  }
+    backgroundColor: '#C33B36',
+    width: '40%',
+    height: 140,
+    borderRadius: 13,
+    marginBottom: 30,
+    alignItems: 'center',
+  },
+  bottom_button_text: {
+    color: '#FFF',
+    fontSize: 25,
+    // letterSpacing: 10,
+    fontWeight: 'bold'
+  },
 })
